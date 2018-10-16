bea0db662555ed8ec6300a0e72839d030c1bf67e
Yacine BOUSLAHI <yacine.bouslahi@gmail.com>
2018-10-16 13:50:41 +0200

    Update easymail.py

--- easymail.py
+++ easymail.py
@@ -1,7 +1,6 @@
 import time, smtplib
  
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
  
 class EasyMail(object):
@@ -10,28 +9,14 @@ class EasyMail(object):
             pass
 
         @classmethod
-        def login(cls, user, password):
+        def login(cls, smtp_server='smtp.gmail.com', user, password):
             instantiate = cls()
-            instantiate.set_server(user, password)
-            return instantiate
-
-        def set_server(self, user, password):
             self.smtp_user = user
             self.smtp_pass = password
-            smtp = {'gmail': 'smtp.gmail.com',
-                    'hotmail': 'smtp.live.com',
-                    'outlook': 'smtp.live.com',
-                    'yahoo': 'smtp.mail.yahoo.com',
-                    }
-            if 'yahoo' in user:
-                self.smtp_server = smtp['yahoo']
-            elif 'gmail' in user:
-                self.smtp_server = smtp['gmail']
-            elif 'hotmail' or 'outlook' in user:
-                self.smtp_server = smtp['hotmail']
-
-
-        def send(self, to, subject, text, path='default'):
+            self.smtp_server = smtp_server
+            return instantiate
+           
+        def send(self, to, subject, text):
             try:
                 msg = MIMEMultipart()
                 text = MIMEText(text, 'html')
@@ -39,21 +24,16 @@ class EasyMail(object):
                 msg['To'] = to
                 msg['From'] = self.smtp_user
                 msg.attach(text)
-                if 'default' not in path:
-                    fp = open(path, 'rb')
-                    img = MIMEImage(fp.read())
-                    fp.close()
-                    img.add_header('Content-ID', path)
-                    msg.attach(img)
-                print "sujeto:%s origen: %s destino: %s servidor de smtp: %s"%(subject, to, self.smtp_user, self.smtp_server)
-                print 'conectando a servidor'
+
+                print "subjet:%s from: %s to: %s smtp server : %s"%(subject, to, self.smtp_user, self.smtp_server)
+                print 'connection to server'
                 s = smtplib.SMTP_SSL(self.smtp_server)
-                print 'logueando...'
+                print 'authentification...'
                 s.login(self.smtp_user, self.smtp_pass)
-                print 'logeado: enviando mail...'
+                print 'connection established : sending mail...'
                 s.sendmail(self.smtp_user, [to], msg.as_string())
                 s.quit()
-                print "Mail enviado"
+                print "message sent"
                 return True
             except Exception as e:
                 print e
