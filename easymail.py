9302497201d8d85a71656b999ba1348ea8dd4eb5
Yacine BOUSLAHI <yacine.bouslahi@gmail.com>
2018-10-16 14:22:03 +0200

    Update easymail.py

--- easymail.py
+++ easymail.py
@@ -1,20 +1,13 @@
 import time, smtplib
- 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
  
 class EasyMail(object):
         
-        def __init__(self):
-            pass
-
-        @classmethod
-        def login(cls, smtp_server='smtp.gmail.com', user, password):
-            instantiate = cls()
+        def __init__(self, user, password, smtp_server = 'smtp.gmail.com'):
             self.smtp_user = user
             self.smtp_pass = password
             self.smtp_server = smtp_server
-            return instantiate
            
         def send(self, to, subject, text):
             try:
@@ -25,16 +18,16 @@ class EasyMail(object):
                 msg['From'] = self.smtp_user
                 msg.attach(text)
 
-                print "subjet:%s from: %s to: %s smtp server : %s"%(subject, to, self.smtp_user, self.smtp_server)
-                print 'connection to server'
+                print("subjet:%s from: %s to: %s smtp server : %s"%(subject, to, self.smtp_user, self.smtp_server))
+                print('connection to server')
                 s = smtplib.SMTP_SSL(self.smtp_server)
-                print 'authentification...'
+                print ('authentification...')
                 s.login(self.smtp_user, self.smtp_pass)
-                print 'connection established : sending mail...'
+                print ('connection established : sending mail...')
                 s.sendmail(self.smtp_user, [to], msg.as_string())
                 s.quit()
-                print "message sent"
+                print ("message sent")
                 return True
             except Exception as e:
-                print e
-
+                print (e)
+              
